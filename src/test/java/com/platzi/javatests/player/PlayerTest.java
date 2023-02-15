674a2eaa19d3a80b7fed51b228b60be14de5f42f
Diego <dportillodeveloper@gmail.com>
2023-02-15 14:50:48 -0300

    PaymentTests

--- src/test/java/com/platzi/javatests/player/PlayerTest.java
+++ src/test/java/com/platzi/javatests/player/PlayerTest.java
@@ -1,7 +1,25 @@
 package com.platzi.javatests.player;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+
+    @Test
+    public void loses_when_dice_number_is_too_low(){
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(2);
+        Player player = new Player(dice, 2);
+        assertFalse(player.play());
+    }
+    @Test
+    public void loses_when_dice_number_is_too_high(){
+        Dice dice = Mockito.mock(Dice.class);
+        Mockito.when(dice.roll()).thenReturn(4);
+        Player player = new Player(dice, 3);
+        assertTrue(player.play());
+    }
 
 }
